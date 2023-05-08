5a896905fbb833812bbab416cbace89074b1cbff
melibul <132861278+melibul@users.noreply.github.com>
2023-05-07 22:59:51 -0300

    Add files via upload
    Ejercicios

---
+++ integrador.py
@@ -0,0 +1,9 @@
+from materias import *
+
+class alumno():
+	def __init__(self, dni, ap,nomb,carr,año):
+	self.__dni=dni
+	self.__apellido=ap
+	self.__nombre=nomb
+	self.__carrera=carr
+	self.__año=año
